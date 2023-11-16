a7ebe75b258dece798bc8452b4262c5bb76ccbfa
eihapKhalid <eihapinfo@gmail.com>
2023-11-16 16:16:02 +0300

    Use the differential equation to make the lighting adapt to the new value

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,16 @@ void setup() {
 }
 
 void loop() {
+  /********** LDR Sensor reading ***********************/
   float CurrentSensorValue = analogRead(sensorPin); // get data From Sensor
   CurrentSensorValue = map(CurrentSensorValue, 0, 1023, 0, 255); // تحويل قراءة الحساس إلى نطاق 0-255
+
+  /********** Use the differential equation to make the lighting adapt to the new value ***********************/
+  // dx/dt = k * (Target - x) ===> -Ln(Target - x) + c2 = Kt + c1 ===> x = Target - e^ (-Kt-c)
+  float Target = CurrentSensorValue;// جعل الهدف هو القيمة الحالية للحساس
+  float k = 0.1; // ثابت النسبة
+  long t = millis() / 1000; // الزمن بالثواني == > 1 ثانية
+  float C = abs(CurrentSensorValue - lastSensorValue); //  حساب الثابت  بناءً على الفرق بين القراءتين الحالية والسابقة وتجنب القيم السالبة
+  lastSensorValue = CurrentSensorValue; // تحديث آخر قيمة تم قراءتها ووضعها كأخر قيمة تم قرائتها بواسطة للحساس
+  int x = Target - exp(-k * t - C); // حساب شدة الضوء بناءً على الصيغة الرياضية
 }
