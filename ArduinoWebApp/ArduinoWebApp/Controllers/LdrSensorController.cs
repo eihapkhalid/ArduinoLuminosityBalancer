c874a5624397edbea32ce4eec06e4b510ea9119e
Eihap Khalid <Eihapinfo@gmail.com>
2023-11-18 19:24:24 +0300

    add SaveSensorReading func to LdrSensorController

--- ArduinoWebApp/ArduinoWebApp/Controllers/LdrSensorController.cs
+++ ArduinoWebApp/ArduinoWebApp/Controllers/LdrSensorController.cs
@@ -1,41 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArduinoWebApp.Acsses.UnitOfWork;
+using ArduinoWebApp.library;
+using ArduinoWebApp.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArduinoWebApp.Controllers
 {
     public class LdrSensorController : Controller
     {
+        private SerialPortConnector _serialPortConnector;
+        private IUnitOfWork _unitOfWork;
+        public LdrSensorController(IUnitOfWork unitOfWork, SerialPortConnector serialPortConnector)
+        {
+            _serialPortConnector =  serialPortConnector;
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
+            var data = _serialPortConnector.Receive(); // استقبال البيانات من الأردوينو
+            var lightReadings = ParseDataToArray(data);
+            var targetLightLevel = CalculateTargetLightLevel(lightReadings, GetCurrentTimeOfDay());
+            _serialPortConnector.Send(targetLightLevel.ToString()); // إرسال القيمة المستهدفة للأردوينو
+
+            // إضافة القيم المتوسطة إلى قاعدة البيانات
+            SaveSensorReading(lightReadings.Average(), DateTime.Now);
+
             return View();
         }
 
+        private void SaveSensorReading(float averageValue, DateTime readingTime)
+        {
+            var sensorReading = new TbLdrSensorReading
+            {
+                LdrSensorReadingAvratgeValue = averageValue,
+                LdrSensorReadingTime = readingTime
+            };
+            _unitOfWork.TbLdrSensorReading.Add(sensorReading);
+            _unitOfWork.Save();
+        }
+        private float[] ParseDataToArray(string data)
+        {
+            // تحويل البيانات النصية إلى مصفوفة أرقام
+            return data.Split(',').Select(float.Parse).ToArray();
+        }
 
+        private TimeOfDay GetCurrentTimeOfDay()
+        {
+            // تحديد الوقت الحالي من اليوم
+            var hour = DateTime.Now.Hour;
+            if (hour < 12)
+                return TimeOfDay.Morning;
+            if (hour < 18)
+                return TimeOfDay.Afternoon;
+            return hour < 22 ? TimeOfDay.Evening : TimeOfDay.Night;
+        }
 
         public int CalculateTargetLightLevel(float[] lightReadings, TimeOfDay timeOfDay)
         {
-            // حساب متوسط قراءات الإضاءة
             float averageLightLevel = lightReadings.Average();
-
-            // تحديد القيمة المثالية للإضاءة بناءً على الوقت من اليوم
             int desiredLightLevel = DetermineDesiredLightLevel(timeOfDay);
 
-            // تعديل الإضاءة بناءً على المتوسط
-            int targetLightLevel;
-            if (averageLightLevel < desiredLightLevel)
-            {
-                targetLightLevel = IncreaseLightLevel(averageLightLevel, desiredLightLevel);
-            }
-            else
-            {
-                targetLightLevel = DecreaseLightLevel(averageLightLevel, desiredLightLevel);
-            }
+            int targetLightLevel = averageLightLevel < desiredLightLevel
+                ? IncreaseLightLevel(averageLightLevel, desiredLightLevel)
+                : DecreaseLightLevel(averageLightLevel, desiredLightLevel);
 
             return Math.Clamp(targetLightLevel, 0, 255);
         }
 
         private int DetermineDesiredLightLevel(TimeOfDay timeOfDay)
         {
-            // تحديد القيمة المثالية للإضاءة بناءً على الوقت من اليوم
             switch (timeOfDay)
             {
                 case TimeOfDay.Morning:
@@ -51,21 +83,22 @@ namespace ArduinoWebApp.Controllers
 
         private int IncreaseLightLevel(float current, int desired)
         {
-            // المعادلة التفاضلية: dx/dt = 0.1 * (desired - current)
-            // هذه المعادلة تستخدم لزيادة مستوى الإضاءة
-            float delta = desired - current;
-            float rateOfChange = delta * 0.1f;
-            return (int)(current + rateOfChange);
+            // معادلة تفاضلية لزيادة مستوى الإضاءة
+            // dx/dt = k * (desired - current)
+            float k = 0.1f;
+            float dx_dt = k * (desired - current);
+            return (int)(current + dx_dt);
         }
 
         private int DecreaseLightLevel(float current, int desired)
         {
-            // المعادلة التفاضلية: dx/dt = 0.1 * (current - desired)
-            // هذه المعادلة تستخدم لتقليل مستوى الإضاءة
-            float delta = current - desired;
-            float rateOfChange = delta * 0.1f;
-            return (int)(current - rateOfChange);
+            // معادلة تفاضلية لتقليل مستوى الإضاءة
+            // dx/dt = -k * (current - desired)
+            float k = 0.1f;
+            float dx_dt = -k * (current - desired);
+            return (int)(current + dx_dt);
         }
+
     }
 
     public enum TimeOfDay
@@ -76,4 +109,3 @@ namespace ArduinoWebApp.Controllers
         Night
     }
 }
-
