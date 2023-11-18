d57aee3f8ff83880049a9d5ffae2d50dd8523fc6
eihapKhalid <eihapinfo@gmail.com>
2023-11-18 15:04:10 +0300

    حذف جميع الملفات من الفرع aspnet-branch

--- include/LEDControl.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEDControl_h
-#define LEDControl_h
-
-#include <Arduino.h>
-const int ledPin = 9; // رقم الرجل الخاص بـ LED
-// تعريف الوظائف والمتغيرات العامة للتحكم في الـ LED
-void controlLED(int sensorValue);
-
-#endif
