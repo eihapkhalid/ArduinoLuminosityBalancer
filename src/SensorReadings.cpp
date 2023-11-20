47e4dba5658f10fdb99513499aaa09b88b7a83e1
eihapKhalid <eihapinfo@gmail.com>
2023-11-20 17:01:05 +0300

    update SensorReadings.h

--- src/SensorReadings.cpp
+++ src/SensorReadings.cpp
@@ -1,6 +1,6 @@
 #include "SensorReadings.h"
 
-const int numReadings = 60; // عدد القراءات لتخزينها لمدة 1 دقائق (فرضًا قراءة كل ثانية)
+const int numReadings = 30; // عدد القراءات لتخزينها لمدة 1 دقائق (فرضًا قراءة كل ثانية)
 float readings[numReadings]; // مصفوفة لتخزين القراءات
 
 // استدعاء الدالة كل ثانية
@@ -13,7 +13,8 @@ void readSensor() {
 //استدعاء الدالة كل دقيقة
 void sendReadings() {
   for (int i = 0; i < numReadings; i++) {
-    Serial.println(readings[i]); // إرسال القراءات عبر الاتصال السيريال
+    Serial.print(readings[i]);Serial.print(','); // إرسال القراءات عبر الاتصال السيريال // إرسال القراءات عبر الاتصال السيريال
   }
+  Serial.println(""); // إرسال القراءات عبر الاتصال السيريال
   readingIndex = 0; // إعادة تعيين المؤشر لبدء تخزين جديد
 }
